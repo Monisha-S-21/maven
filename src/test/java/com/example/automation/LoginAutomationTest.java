c0800533f384c2918f705b43a3639a892637925c
Monisha-S-21 <125334076+Monisha-S-21@users.noreply.github.com>
2025-01-01 16:20:51 +0530

    Update LoginAutomationTest.java

--- src/test/java/com/example/automation/LoginAutomationTest.java
+++ src/test/java/com/example/automation/LoginAutomationTest.java
@@ -12,7 +12,7 @@ public class LoginAutomationTest {
         // Set up WebDriver
         System.setProperty("webdriver.chrome.driver", "C:/Program Files/chromedriver-win64/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
-
+        options.addArguments("--remote-debugging-port=9222");
         WebDriver driver = new ChromeDriver(options);
 
         try {
