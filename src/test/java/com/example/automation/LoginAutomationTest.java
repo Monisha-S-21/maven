3f9004147f53b0da44b16f2cab10af4d2b4ca85e
Monisha-S-21 <125334076+Monisha-S-21@users.noreply.github.com>
2025-01-01 14:59:25 +0530

    Update LoginAutomationTest.java

--- src/test/java/com/example/automation/LoginAutomationTest.java
+++ src/test/java/com/example/automation/LoginAutomationTest.java
@@ -9,7 +9,7 @@ public class LoginAutomationTest {
     @Test
     public void testLogin() {
         // Set up WebDriver
-        System.setProperty("webdriver.chrome.driver", "C:/Program Files/chromedriver-win64");
+        System.setProperty("webdriver.chrome.driver", "C:/Program Files/chromedriver-win64/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         try {
